85ee28d4ad08edc81efa4e7adac2171521c0ca8f
YangYangXD <101124290+YangYangXD@users.noreply.github.com>
2024-05-05 22:53:43 +0800

    Add files via upload

---
+++ Main.gd
@@ -0,0 +1,56 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+
+func _ready():
+	randomize()
+
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+func _on_MobTimer_timeout():
+	
+	var mob = mob_scene.instance()
+	
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	mob.position = mob_spawn_location.position
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+	
+
